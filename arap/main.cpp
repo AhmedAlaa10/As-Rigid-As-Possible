2f6207b1f411f57595980321dcf63c3aa25f79e1
Michael Loipführer <michael.loipfuehrer@stusta.de>
2021-01-09 15:59:11 +0100

    initial working cmake setup

---
+++ arap/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <Eigen/Dense>
+#include <Eigen/Sparse>
+#include <igl/cotmatrix.h>
+#include <ceres/ceres.h>
+
+
+int main() {
+    Eigen::MatrixXd V(4, 2);
+    V << 0, 0,
+            1, 0,
+            1, 1,
+            0, 1;
+    Eigen::MatrixXi F(2, 3);
+    F << 0, 1, 2,
+            0, 2, 3;
+    Eigen::SparseMatrix<double> L;
+    igl::cotmatrix(V, F, L);
+    std::cout << "Hello, mesh: " << std::endl << L * V << std::endl;
+    return 0;
+}
