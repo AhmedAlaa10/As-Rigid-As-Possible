d388bda8f8d537faed40af25981b761ac72b605d
Michael Loipführer <michael.loipfuehrer@stusta.de>
2021-01-09 16:07:15 +0100

    viewer example + correct bunny mesh

--- arap/main.cpp
+++ arap/main.cpp
@@ -1,21 +1,16 @@
-#include <iostream>
-#include <Eigen/Dense>
-#include <Eigen/Sparse>
-#include <igl/cotmatrix.h>
-#include <ceres/ceres.h>
+#include <igl/readOFF.h>
+#include <igl/opengl/glfw/Viewer.h>
 
 
 int main() {
-    Eigen::MatrixXd V(4, 2);
-    V << 0, 0,
-            1, 0,
-            1, 1,
-            0, 1;
-    Eigen::MatrixXi F(2, 3);
-    F << 0, 1, 2,
-            0, 2, 3;
-    Eigen::SparseMatrix<double> L;
-    igl::cotmatrix(V, F, L);
-    std::cout << "Hello, mesh: " << std::endl << L * V << std::endl;
-    return 0;
+    Eigen::MatrixXd V;
+    Eigen::MatrixXi F;
+
+    // load the mesh in off format
+    igl::readOFF("../data/bunny.off", V, F);
+
+    // show the mesh in the igl viewer
+    igl::opengl::glfw::Viewer viewer;
+    viewer.data().set_mesh(V, F);
+    viewer.launch();
 }
